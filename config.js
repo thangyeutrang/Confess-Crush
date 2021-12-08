8802206dbf0ded70da9499b225367d882eeaf9fa
thangyeutrang <95753252+thangyeutrang@users.noreply.github.com>
2021-12-08 20:36:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi tới chagg",
+    introTitle: 'chan',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnIntro: '^^iu^^',
+    title: 'Anh đã đi theo e rất lâu mặc dù anh điều đó là sai 🥰',
+    desc: 'Môi em như mật ngọt miệng em nói như rót vào tai ',
+    btnYes: 'iu khum nè <33',
+    btnNo: 'khum nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người nhưng nụ cười của em vẫn là đẹp nhứt <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'ngáo <33333333',
+    mess: 'tui biết mà. kiủ gì cũng kiu ngáo nì',
+    messDesc: 'Nghe tui noái nài từ từ gòi yêu đc khum , xíu inbox tui nho.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
